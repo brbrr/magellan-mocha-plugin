02c364545e9ca27a50cb3cf122aa6ec0d8ac4843
Scott Stancil <scott.stancil@automattic.com>
2017-04-18 11:05:27 -0400

    Added tests

---
+++ test_support/suite/spec.js
@@ -0,0 +1,11 @@
+describe('Suite @suite', function() {
+	it('passes', function() {});
+	it('passes', function() {});
+	it('passes', function() {});
+});
+describe('Suite 2 @suite', function() {
+	it('passes', function() {});
+});
+describe('Suite 3 (excluded)', function() {
+	it('passes', function() {});
+});
